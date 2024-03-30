78c6b78bc6c4dffad8ad283225d99f103d19f7a4
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2024-03-29 21:26:20 -0300

    Add Card Component

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,22 +1,14 @@
 import React from "react";
 
-
-const Card = ({ name, username, id }) => {
-
-  const addFav = ()=>{
-    // Aqui iria la logica para agregar la Card en el localStorage
-  }
-
+const Card = () => {
   return (
-    <div className="card">
-        {/* En cada card deberan mostrar en name - username y el id */}
-
-        {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
-
-        {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
-        <button onClick={addFav} className="favButton">Add fav</button>
+    <div>
+      <img src="../../public/images/doctor.jpg" alt="" />
+      <h4>NAME</h4>
+      <h4>USERNAME</h4>
+      <button>Eliminar favorito ❌</button>
+      <button>Agregar a favoritos⭐</button>
     </div>
   );
 };
-
 export default Card;

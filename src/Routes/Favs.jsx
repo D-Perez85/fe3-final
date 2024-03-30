2fd68e884ca2b6a6b3c22ddc5af1c1157821ec1b
D-Perez85 <77124855+D-Perez85@users.noreply.github.com>
2024-03-29 23:34:08 -0300

    Add Favs (Styles + Render)

--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "../Components/Card";
+import { useContextGlobal } from "../Components/utils/global.context";
 import Button from "../Components/Button";
 
 const Favs = () => {
+  const { state, dispatch } = useContextGlobal();
+  console.log(state.favs.length);
+  const [isFav, setIsFav] = useState(true);
+  
+  const handleRemoveAll = () => {
+    dispatch({ type: "REMOVE_ALL_FAVS" });
+    localStorage.removeItem("favs");
+  };
+  
+  const handleRemoveSingle = (id) => {
+    dispatch({ type: "REMOVE_SINGLE_FAV", payload: id });
+    const updatedFavs = state.favs.filter((dentist) => dentist.id !== id);
+    localStorage.setItem("favs", JSON.stringify(updatedFavs));
+  };
+
   return (
     <>
       <h1>Destacados</h1>
-      <div>
-        <Card />
+      <div className="card-grid">
+        {state.favs.map((item) => (
+          <Card key={item.id} item={item} isFav={isFav}
+            handleRemoveSingle={handleRemoveSingle}/>))}
       </div>
-      <p>Ud. no tiene favoritos agregados aún!</p>
-      <button>BORRAR TODOS</button>
+      {state.favs != 0 ? ("") : 
+      (
+        <p className="p-destacados">Ud. no tiene favoritos agregados aún!</p>)}
+      {state.favs.length >= 2 ? (
+        <button onClick={handleRemoveAll} className="button-delAll">
+          BORRAR TODOS
+        </button>) : ("")}
       <Button />
     </>
   );
